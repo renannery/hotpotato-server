7a6919f6d3843e3714df075febc249ac27ed6227
Renan Nery <renannery@gmail.com>
2015-12-10 23:12:19 -0200

    Implemented random potato creation flow, but still WIP because of nested callback

--- public/js/main.js
+++ public/js/main.js
@@ -11,9 +11,7 @@ window.onload = function() {
     });
 
     $('#btn-send-potato').click(function() {
-        for (var i = 1; i <= 3; i++) {
-            setTimeout(createPotatoAndSend, 5000 * i);
-        }
+        createPotatoAndSend();
     });
 
     function createPotatoAndSend() {
@@ -24,7 +22,6 @@ window.onload = function() {
             secondsToBurn: Math.floor(Math.random() * 60) + 10
         }, function(result, error) {
             if (error) {
-                // An error could be either the err.message property, or err.errors.title, so we account for either case
                 alert(err.message || (err.errors && err.errors.title));
                 return;
             }
@@ -35,31 +32,25 @@ window.onload = function() {
 
     function connect(message) {
 
-        jid = "renannery10"
+        jid = "jean10"
         var password = "Senha2015"
-        var messageTo = "";
 
-        //An example of bosh server. This site is working but it can change or go down.
-        //If you are going to have a production site, you must install your own BOSH server
         var url = "http://jabber.rootbash.com:7070/http-bind/";
         $.xmpp.connect({
             url: url,
             jid: jid,
             password: password,
             onConnect: function() {
-                alert("Connected " + message);
+
                 sendMessage(message);
                 $.xmpp.setPresence(null);
             },
-            onPresence: function(presence) { },
+            onPresence: function(presence) {},
             onDisconnect: function() {
                 alert("Disconnected");
             },
 
-            onMessage: function(message) {
-                var user = message.from.split("@", 1);
-                alert(user + "\n" + message.body);
-            },
+            onMessage: function(message) {},
             onError: function(error) {
                 alert(error.error);
             }
@@ -67,17 +58,26 @@ window.onload = function() {
     }
 
     function sendMessage(message) {
-        var messageTo;
-        messageTo = "ursinho@jabber.rootbash.com";
-        // if (jid == "renannery10") {
-            
-        // } else {
-        //     messageTo = "renannery10@jabber.rootbash.com";
-        // }
+        //TODO How can I deal with callbacks
+        getRandomUser(message);
 
-        $.xmpp.sendMessage({
-            body: message,
-            to: messageTo
+    }
+
+    function getRandomUser(message) {
+        dpd.users.get(function(users, error) {
+            if (error) {
+                alert(error.message);
+                return;
+            }
+
+            var index = Math.floor(Math.random() * users.length) + 0;
+            var username = users[index].chatUsername;
+            var messageTo = username + "@jabber.rootbash.com";
+            alert("Send " + message + " to " + username);
+            $.xmpp.sendMessage({
+                body: message,
+                to: messageTo
+            });
         });
     }
 };
